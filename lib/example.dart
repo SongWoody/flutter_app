2b64afeeae6662b0a19e9e2c180b17cac80a374e
Woody <syg1138@gmail.com>
2021-07-24 19:18:15 +0900

    example async/await

--- lib/example.dart
+++ lib/example.dart
@@ -1,27 +1,55 @@
 
 
+import 'package:flutter/foundation.dart';
+
 /// ExampleMain
 void exampleMain() {
   print("Start exampleMain");
 
-  numberFuture().then((value) => {
-    print("value: $value")
-  }).catchError((error){
-    print("error: $error");
+  // numberFuture().then((value) => {
+  //   print("value: $value")
+  // }).catchError((error){
+  //   print("error: $error");
+  // });
+
+  login().then((value) => {
+    print("login $value}")
   });
 }
 
 /// convention
 /// enum 은 UpperCamelCase
 /// enum member 는 LowCamelCase
-enum Status {
-  login,
-  logout
+enum LoginStatus {
+  success,
+  failure
 }
 
-/// Future Example
-Future<int> numberFuture() {
-  return Future<int>.delayed(Duration(seconds: 5) , () {
-    return 100;
-  });
+/// async / await Example
+Future<LoginStatus> login() async{
+  var id = await Disk().readUserId();
+  var result = await LoginManager().login(id);
+  return result ? LoginStatus.success : LoginStatus.failure;
+}
+
+class Disk {
+  Future<String> readUserId() {
+    print("read id from disk");
+    return Future.delayed(Duration(seconds: 3), () {
+      return "Woody";
+    });
+  }
+}
+
+class LoginManager {
+  Future<bool> login(String id) {
+    print("login process");
+    return Future.delayed(Duration(seconds: 1), () {
+      if (id == "Woody") {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  }
 }
