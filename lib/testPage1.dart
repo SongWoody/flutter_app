5475aed9e865aee4308734af5e44e506a8c66080
Woody <syg1138@gmail.com>
2021-07-26 22:52:17 +0900

    add dialog in testPage1

--- lib/testPage1.dart
+++ lib/testPage1.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class TestPage1 extends StatelessWidget {
@@ -6,10 +5,40 @@ class TestPage1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      color: Colors.red,
+    final _nameController = TextEditingController();
+    return Material(
+      child: Scaffold(
+        appBar: AppBar(title: Text(this.toString())),
+        body: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: "이름을 입력해 주세요."),
+              controller: _nameController,
+            ),
+            SizedBox(height: 20),
+            Text("hi", style: TextStyle(fontSize: 30),)
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // print("name: ${_nameController.text}");
+            return showMyDialog(context, _nameController.text);
+          },
+        ),
+      ),
+    );
+  }
+
+  dynamic showMyDialog(BuildContext context, String text) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            content: Text(
+              'Message $text',
+            ),
+          );
+        }
     );
   }
 }
