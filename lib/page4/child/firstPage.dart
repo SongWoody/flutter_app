e275eb06c2b4301fd7b13f1a68d7934a955f4408
Woody <syg1138@gmail.com>
2021-08-03 17:08:04 +0900

    메뉴 구성

--- lib/page4/child/firstPage.dart
+++ lib/page4/child/firstPage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class FirstPage extends StatelessWidget {
@@ -6,7 +7,93 @@ class FirstPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("First Page"),
+      child: Column(
+        children: [
+          _buildTop(),
+          _buildMiddle(),
+          _buildBottom(),
+        ],
+      ),
     );
+  }
+
+  Widget _buildTop() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20, bottom: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, ),
+                  Text("택시"),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, ),
+                  Text("택시"),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, ),
+                  Text("택시"),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, ),
+                  Text("택시"),
+                ],
+              ),
+            ],
+          ),
+          SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, ),
+                  Text("택시"),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, ),
+                  Text("택시"),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(Icons.local_taxi, ),
+                  Text("택시"),
+                ],
+              ),
+              Opacity(
+                opacity: 0,
+                child: Column(
+                  children: [
+                    Icon(Icons.local_taxi, ),
+                    Text("택시"),
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMiddle() {
+    return Text("Middle");
+  }
+
+  Widget _buildBottom() {
+    return Text("Bottom");
   }
 }
