0b7f5a915ecc8d901205257b727d9e33a981c8cb
Woody <syg1138@gmail.com>
2021-08-13 13:07:38 +0900

    example Json

--- lib/page8/testPage8.dart
+++ lib/page8/testPage8.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 /// Json Test Page
 class TestPage8 extends StatelessWidget {
@@ -8,9 +9,53 @@ class TestPage8 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String objText = '{ "name":"woody", "age"'':29 }';
+    User? woody;
+    try {
+      woody = User.fromJson(jsonDecode(objText));
+    } catch(e) {
+      print("catch");
+    }
+    if (woody != null) {
+      print('${woody.name}');
+      print('${woody.age}');
+    }
+
     return Scaffold(
       appBar: AppBar(title: Text("Json Test Page"),),
-      body: Container(),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Text("name: "),
+              Text("${woody?.name}")
+            ],
+          ),
+          Row(
+            children: [
+              Text("age: "),
+              Text("${woody?.age}")
+            ],
+          ),
+
+        ],
+      ),
     );
+  }
+}
+
+class User {
+  String name;
+  int age;
+
+  User(this.name, this.age);
+
+  factory User.fromJson(dynamic json) {
+    return User(json['name'] as String, json['age'] as int);
+  }
+
+  @override
+  String toString() {
+    return '{ name:${this.name}, age:${this.age} }';
   }
 }
